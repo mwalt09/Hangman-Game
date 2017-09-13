52ae9e1ead5ee64e4a6553ee962dd64db8d97daa
Mikael Walters <mikael.walters@gmail.com>
2017-09-13 01:23:34 -0500

    fixed bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,130 +9,125 @@ var phraseLength = 0;
 var active = true;
 var wordsIndex = 0;
 var wordBank = [
-	"alien",
-	"dean",
-	"cars",
-	"jackie",
-	"sing",
-	"paterson",
-	"arrival",
-	"zootopia",
-	"moonlight",
-	"la la land",
-	"rogue one: a star wars story"
+  "alien",
+  "cars",
+  "jackie",
+  "sing",
+  "paterson",
+  "arrival",
+  "zootopia",
+  "moonlight",
+  "la la land",
+  "rogue one: a star wars story"
+];
+var hintBank = [
+  "Stars Sigourney Weaver as Ripley...",
+  "Lightning McQueen...",
+  "Natalie Portman plays JFK's wife in...",
+  "Matthew McConaughey plays a Koala in...",
+  "Adam Driver plays a bus driver and poet in this 2016 film...",
+  "Alien language that allows you to see the past and future...",
+  "A mammal metropolis where various animals live and thrive...",
+  "This film presents three stages in the life of the main character...",
+  "Musical comedy staring Ryan Gosling and Emma Stone...",
+  "A 2016 American space epic film directed by Gareth Edwards..."
 ];
 var word = "";
+var hint = "";
 var display = "";
-var state ="start";
+var state = "start";
 var inProgress = [];
+var blankCounter;
 
 // Functions
 // ===========================================================
 
-
 function getWord() {
-	var index = Math.floor(Math.random() * wordBank.length);
-	word = wordBank[index];
-	display = word.replace(/[a-z,A-Z]/g, "_");
-	document.getElementById("display").innerHTML = display;
-	console.log(word);
-}
-
-getWord();
-
-function spaceCounter() {
-	var blankCounter = display.length;
-	console.log(blankCounter);
+  var index = Math.floor(Math.random() * wordBank.length);
+  word = wordBank[index];
+  hint = hintBank[index];
+  wordBank.splice(index, 1);
+  console.log(wordBank);
+  hintBank.splice(index, 1);
+  display = word.replace(/[a-z,A-Z]/g, "_");
+  document.getElementById("display").innerHTML = display;
+  document.getElementById("hint").innerHTML = hint;
+  wordsIndex++;
+  blankSpaces();
 }
 
 function updateWins() {
-	document.getElementById("wins").innerHTML = "Wins: " + wins;
+  document.getElementById("wins").innerHTML = "Wins: " + wins;
 }
 
 function updateGuesses() {
-	document.getElementById("guesses").innerHTML = guesses;
+  document.getElementById("guesses").innerHTML = guesses;
 }
 
 function resetRound() {
-	guesses = maxGuesses;
-	lettersGuessed = [];
-	document.getElementById("letters").innerHTML = "";
-	inProgress = [];
-	// document.getElementById("gameOver").innerHTML = "";
-	document.getElementById("currentWord").innerHTML = "";
-	randomizer();
+  if (wordBank.length === 0 || guesses === 0) {
+    didWin();
+  } else {
+    guesses = maxGuesses;
+    lettersGuessed = [];
+    document.getElementById("letters").innerHTML = "";
+    inProgress = [];
+    getWord();
+  }
 }
 
+function blankSpaces() {
+  blankCounter = display.match(/_/g);
+}
 
+function didWin() {
+  document.getElementById("gameOver").innerHTML = "GAME OVER!";
+  document.getElementById("wins").innerHTML =
+    "You solved: " + wins + " out of 10.";
+}
+
+getWord();
 updateGuesses();
-spaceCounter();
 
 // Game Play
 // ==========================================================
 window.onload = function() {
-	
+  document.onkeyup = function(event) {
+    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 
+    if (userInput) document.getElementById("gameOver").innerHTML = "";
 
-document.onkeyup = function(event) {
+    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 
-	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    for (var i = 0; i < word.length; i++) {
+      var firstChar = word[i].indexOf(userInput);
+      if (firstChar !== -1) {
+        display = display.slice(0, i) + userInput + display.slice(i + 1);
+        document.getElementById("display").innerHTML = display;
+      }
+    }
 
-		document.getElementById("gameOver").innerHTML = "";
-		
-		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    function updateLettersGuessed() {
+      if (display.indexOf(userInput) === -1) {
+        lettersGuessed.push(userInput);
+        guesses--;
+      } else {
+		lettersGuessed.push(userInput);
+        blankSpaces();
+      }
 
-		for (var i = 0; i < word.length; i++) {
-			var firstChar = word[i].indexOf(userInput);
-			if (firstChar !== -1) {
-				// alert("yes " + i);
-				display = display.slice(0, i) + userInput + display.slice(i + 1);
-				document.getElementById("display").innerHTML = display;
-		}
-	}
+      for (var i = 0; i < lettersGuessed.length; i++) {
+        document.getElementById("letters").innerHTML = lettersGuessed;
+      }
 
-		// var theIndex = display.indexOf(userInput);
-		// inProgress[theIndex] = userInput;
-		// document.getElementById("currentWord").innerHTML = inProgress;
+      if (blankCounter === null) {
+        wins++;
+        updateWins();
+        resetRound();
+      }
 
-	function updateLettersGuessed() {
-		if (display.indexOf(userInput) === -1) {
-		// if (lettersGuessed.indexOf(userInput) == -1) {
-			lettersGuessed.push(userInput);
-			guesses--;
-		} else {
-			blankCounter--;
-			// console.log(counter);
-		}
-
-		for (var i = 0; i < lettersGuessed.length; i++) {
-			document.getElementById("letters").innerHTML = lettersGuessed;
-		}
-
-		if (blankCounter === 0) {
-			wins++;
-			document.getElementById("gameOver").innerHTML = "YOU WON";
-			document.getElementById("wins").innerHTML = "You solved: " + wins + " out of 10.";
-			updateWins();
-			resetRound();
-		}
-
-		if (wordsIndex > (words.length-1) || (guesses === 0)) {
-			document.getElementById("gameOver").innerHTML = "GAME OVER!";
-			document.getElementById("wins").innerHTML = "You solved: " + wins + " out of 10.";
-		}
-	}
-	updateLettersGuessed();
-	updateGuesses();
-	spaceCounter();
-	
-}
-
+    }
+    updateLettersGuessed();
+    updateGuesses();
+  };
 };
-
-
-
-
-
-
-
-
